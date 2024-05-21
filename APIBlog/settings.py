471574631a4016e3a83495ba48aacd770232b4b3
Rahul Saini <rahulsaini3125@gmail.com>
2024-05-21 16:08:52 +0000

    fix bug

--- APIBlog/settings.py
+++ APIBlog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#9qmy^(ux0nsg1k4pkaf(3#se#)n5y*we6)_j44#sk^#io=os8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- APIBlog/urls.py
+++ APIBlog/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if not settings.DEBUG :
+    urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
